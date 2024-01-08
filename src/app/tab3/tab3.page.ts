12dc37b708ed9d53a5981a6e0bfe6e55972cfdb9
Christian von Rohr <christian.vonrohr@well.ch>
2024-01-08 08:47:58 +0100

    feat: open InAppBrowser on tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Browser, OpenOptions } from '@capacitor/browser';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-
   constructor() {}
 
+  onOpenBrowser() {
+    const config: OpenOptions = {
+      url: 'https://www.google.com',
+      // presentationStyle: 'popover',
+      // dismissButtonStyle: 'done',
+      // preferredBarTintColor: '#453AA4',
+      // preferredControlTintColor: 'white',
+    };
+    Browser.open(config).then(
+      (res: any) => console.log(res),
+      (err: any) => console.log(err)
+    );
+  }
 }
